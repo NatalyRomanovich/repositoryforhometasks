ce7ffc514651f9df9a3097a0859105b65e711c8b
Таиса <taisaqueen@gmail.com>
2022-04-13 23:12:21 +0300

    Initial Commit

--- src/by/nat/homework3/test/TestUser.java
+++ src/by/nat/homework3/test/TestUser.java
@@ -2,7 +2,7 @@ package by.nat.homework3.test;
 
 import by.nat.homework3.domain.User;
 import by.nat.homework3.util.CreateUsers;
-import by.nat.homework3.util.OtherCreateUser;
+
 
 public class TestUser {
 
