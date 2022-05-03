944692c744a598b2e9b234f675d7ccb4fd4acd81
NatalyRomanovich <102173329+NatalyRomanovich@users.noreply.github.com>
2022-05-03 21:07:23 +0300

    Update CreateThreads.java

--- src/by/nat/homework4/util/CreateThreads.java
+++ src/by/nat/homework4/util/CreateThreads.java
@@ -10,46 +10,62 @@ import java.util.concurrent.Executors;
 public class CreateThreads {
 
     //create executor for operator's threads
-    public static void generateExecutorOperators() throws InterruptedException {
+    public static void generateExecutorOperators() {
 
         ExecutorService service = Executors.newCachedThreadPool();
 
         int threadNumber = Information.getInformation(CreateCallCentre.FILE_NAME_CALL_CENTRE,
                 CreateCallCentre.OPERATORS_NUMBER);
 
-        for (int i = 0; i < threadNumber; i++) {
+        try {
+            for (int i = 0; i < threadNumber; i++) {
 
-            Operator callCentreOperator = GeneratorOperators.createOperator(i);
-            service.execute(callCentreOperator);
+                Operator callCentreOperator = GeneratorOperators.createOperator(i);
+                service.execute(callCentreOperator);
 
-            Thread.sleep(Information.getInformation(CreateCallCentre.FILE_NAME_CALL_CENTRE,
-                    CreateCallCentre.BREAKS_BETWEEN_OPERATOR_ACTIVATIONS));
+                Thread.sleep(Information.getInformation(CreateCallCentre.FILE_NAME_CALL_CENTRE,
+                        CreateCallCentre.BREAKS_BETWEEN_OPERATOR_ACTIVATIONS));
+            }
+            service.shutdown();
+        } catch (
+                InterruptedException e) {
+            e.printStackTrace();
         }
-        service.shutdown();
+
     }
 
     //create executor for client's threads
-    public static void generateExecutorClients(CallCentre callCentre) throws InterruptedException {
+    public static void generateExecutorClients(CallCentre callCentre) {
 
         ExecutorService service = Executors.newCachedThreadPool();
 
         int threadNumber = Information.getInformation(GeneratorClients.FILE_NAME_CLIENTS,
                 GeneratorClients.CLIENTS_NUMBER);
 
-        for (int i = 0; i < threadNumber; i++) {
+        try {
+            for (int i = 0; i < threadNumber; i++) {
 
-            Client calledClient = GeneratorClients.createClient(i);
-            service.execute(calledClient);
-            Thread.sleep(CreateCallCentre.customerCalls(callCentre));
+                Client calledClient = GeneratorClients.createClient(i);
+                service.execute(calledClient);
+                Thread.sleep(CreateCallCentre.customerCalls(callCentre));
+            }
+            service.shutdown();
+        } catch (
+                InterruptedException e) {
+            e.printStackTrace();
         }
-        service.shutdown();
     }
 
     //create call centre's thread
-    public static void generateCallCentreThread(CallCentre callCentre) throws InterruptedException {
+    public static void generateCallCentreThread(CallCentre callCentre) {
 
         Thread callCentreThread = new Thread(callCentre);
         callCentreThread.start();
-        Thread.sleep(CreateCallCentre.timeCallCentreWorking(callCentre));
+        try {
+            Thread.sleep(CreateCallCentre.timeCallCentreWorking(callCentre));
+        } catch (
+                InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
